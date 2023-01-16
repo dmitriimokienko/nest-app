f08f91cd6e7b9028efe9598fac411960e32394e7
Dmitrii Mokienko <dmitriimokienko@MacBook-Air-Dmitrii.local>
2023-01-16 22:07:48 +0400

    feat: typeorm config

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dbEnvConfig, { dbConfig } from 'src/core/db.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [dbEnvConfig],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: dbConfig.host,
+      // port: dbConfig.port,
+      username: dbConfig.username,
+      password: dbConfig.password,
+      database: dbConfig.database,
+      entities: ['src/modules/users/user.entity'],
+      logging: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
